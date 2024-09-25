730fcc1dea230477c80a94cd8b4e864561a22900
karl schick <schickperez@gmail.com>
2024-09-25 15:32:43 -0500

    listo modal

--- resources/views/tiendaonline/productos.blade.php
+++ resources/views/tiendaonline/productos.blade.php
@@ -53,7 +53,8 @@
                                 @foreach ($productos as $producto)
                                     <div class="col-md-3 mb-0">
                                         <!-- Enlace para abrir el modal del producto -->
-                                        <a href="#" data-toggle="modal" data-target="#productModal{{ $producto->id }}" style="color:black;">
+                                        <a href="#" data-toggle="modal" data-target="#productModal{{ $producto->id }}"
+                                            style="color:black;">
                                             <div class="card border-dark mb-3 h-100">
                                                 <div class="card-body d-flex flex-column">
                                                     <h4 class="card-title">{{ $producto->nombre_producto }}</h4>
@@ -68,9 +69,9 @@
                                                 <!-- Contenedor para centrar la imagen -->
                                                 <div class="d-flex justify-content-center" style="padding: 10px;">
                                                     <img src="{{ $producto->imagen ? asset('storage/' . $producto->imagen) : asset('images/libros_vent.png') }}"
-                                                    class="card-img-top" alt="Logo del producto"
-                                                    style="height: 90px; object-fit: contain; margin: 10px;">
-                                               
+                                                        class="card-img-top" alt="Logo del producto"
+                                                        style="height: 90px; object-fit: contain; margin: 10px;">
+
                                                 </div>
 
                                                 <div class="card-header mt-auto"
@@ -91,68 +92,89 @@
                                         </a>
                                     </div>
 
-<!-- Modal Reutilizable para los productos -->
-<div class="modal fade" id="productModal{{ $producto->id }}" tabindex="-1" role="dialog" aria-labelledby="modalTitle{{ $producto->id }}" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header" style="background-color: #193e66; color: white;">
-                <h5 class="modal-title" id="modalTitle{{ $producto->id }}">{{ $producto->nombre_producto }}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="color: white;">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <!-- Columna para las opciones de compra -->
-                    <div class="col-md-7 text-center">
-                        <div class="card text-white bg-secondary mb-3 shadow-sm">
-                            <div class="card-header">
-                                <i class="fas fa-fw fa-money-bill-alt fa-2x" aria-hidden="true"></i><br>
-                                {{ $producto->nombre_producto }}<br>
-                            </div>
-                        </div>
+                                    <!-- Modal Reutilizable para los productos -->
+                                    <div class="modal fade" id="productModal{{ $producto->id }}" tabindex="-1"
+                                        role="dialog" aria-labelledby="modalTitle{{ $producto->id }}" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header" style="background-color: #193e66; color: white;">
+                                                    <h5 class="modal-title" id="modalTitle{{ $producto->id }}">
+                                                        {{ $producto->nombre_producto }}</h5>
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                        aria-label="Close" style="color: white;">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="row">
+                                                        <!-- Columna para las opciones de compra -->
+                                                        <div class="col-md-7 text-center">
 
-                        <!-- Opciones de compra -->
-                        @if($producto->valor_unitario_mes != '0')
-                            <a href="/tienda/savecarrito/{{ $producto->id }}" class="card text-white bg-primary mb-3 shadow-sm" style="max-width: 18rem;">
-                                <div class="card-body">
-                                    <p>Valor 1 Mes c/u</p>
-                                    <b>${{ $producto->valor_unitario_mes }} COP</b>
-                                </div>
-                            </a>
-                        @endif
 
-                        @if($producto->valor_seis_meses != '0')
-                            <a href="/tienda/savecarrito/{{ $producto->id }}" class="card text-white bg-primary mb-3 shadow-sm" style="max-width: 18rem;">
-                                <div class="card-body">
-                                    <p>Valor 6 Meses c/u</p>
-                                    <b>${{ $producto->valor_seis_meses }} COP</b>
-                                </div>
-                            </a>
-                        @endif
+                                                            <!-- Mostrar referencia en un card -->
+                                                            <div class="card text-white bg-secondary mb-3 shadow-sm">
+                                                                <div class="card-header">Referencia</div>
+                                                                <div class="card-body">
+                                                                    <h6 class="card-title">{{ $producto->referencia }}</h6>
+                                                                </div>
+                                                            </div>
 
-                        @if($producto->valor_doce_meses != '0')
-                            <a href="/tienda/savecarrito/{{ $producto->id }}" class="card text-white bg-primary mb-3 shadow-sm" style="max-width: 18rem;">
-                                <div class="card-body">
-                                    <p>Valor 12 Meses c/u</p>
-                                    <b>${{ $producto->valor_doce_meses }} COP</b>
-                                </div>
-                            </a>
-                        @endif
-                    </div>
+                                                            <!-- Mostrar descripción en un card -->
+                                                            <div class="card text-white bg-secondary mb-3 shadow-sm">
+                                                                <div class="card-header">Descripción</div>
+                                                                <div class="card-body">
+                                                                    <p class="card-text">
+                                                                        {{ $producto->descripcion_de_producto ?: 'No disponible' }}
+                                                                    </p>
+                                                                </div>
+                                                            </div>
 
-                    <!-- Columna para la imagen -->
-                    <div class="col-md-5 text-center">
-                        <img src="{{ $producto->imagen ? asset('storage/' . $producto->imagen) : asset('images/libro1.png') }}" alt="Product Image" class="img-fluid rounded shadow-sm" style="max-height: 300px;">
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-            </div>
-        </div>
-    </div>
-</div>
+
+                                                        </div>
+
+                                                        <!-- Columna para la imagen -->
+                                                        <div class="col-md-5 text-center">
+                                                            <img src="{{ $producto->imagen ? asset('storage/' . $producto->imagen) : asset('images/libro1.png') }}"
+                                                                alt="Product Image" class="img-fluid rounded shadow-sm"
+                                                                style="max-height: 300px;">
+                                                            <!-- Opciones de compra -->
+                                                            @if ($producto->valor_unitario_mes != '0')
+                                                                <a href="/tienda/savecarrito/{{ $producto->id }}"
+                                                                    class="btn btn-primary mb-2">
+                                                                    Valor 1 Mes: <b>${{ $producto->valor_unitario_mes }}
+                                                                        COP</b>
+                                                                </a>
+                                                            @endif
+
+                                                            @if ($producto->valor_seis_meses != '0')
+                                                                <a href="/tienda/savecarrito/{{ $producto->id }}"
+                                                                    class="btn btn-primary mb-2">
+                                                                    Valor 6 Meses: <b>${{ $producto->valor_seis_meses }}
+                                                                        COP</b>
+                                                                </a>
+                                                            @endif
+
+                                                            @if ($producto->valor_doce_meses != '0')
+                                                                <a href="/tienda/savecarrito/{{ $producto->id }}"
+                                                                    class="btn btn-primary mb-2">
+                                                                    Valor 12 Meses: <b>${{ $producto->valor_doce_meses }}
+                                                                        COP</b>
+                                                                </a>
+                                                            @endif
+
+                                                        </div>
+
+
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-dismiss="modal">Cerrar</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
 
                                     <!-- Fin del Modal para este producto -->
                                 @endforeach
